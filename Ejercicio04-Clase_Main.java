ddae7d6db4ec2a04108934396e11fbff2df3a9c2
ManuelChampi <132849071+ManuelChampi@users.noreply.github.com>
2023-05-23 21:40:09 -0500

    Update and rename Ejercicio04.java to Ejercicio04-Clase_Main.java

---
+++ Ejercicio04-Clase_Main.java
@@ -0,0 +1,18 @@
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        List<String> miLista = new List<>();
+        miLista.AgregarDatos("A");
+        miLista.AgregarDatos("B");
+        miLista.AgregarDatos("C");
+
+        miLista.MostrarDatos();
+    }
+}
+
+/* 
+run:
+A
+B
+C
+BUILD SUCCESSFUL (total time: 0 seconds)
+*/
